451514ac16870e814083df2be6199db3e99ab159
Stefan Nedelchev <hardmaster.92@hotmail.com>
2022-05-05 15:38:28 +0300

    refactoring contacts

--- src/app/contacts/contacts.page.ts
+++ src/app/contacts/contacts.page.ts
@@ -12,25 +12,26 @@ export class ContactsPage {
   public availableProps: string[] = [];
   public selectedProps: string[] = [];
   public contacts: ContactResult[] = [];
-
   public multiple = false;
 
   constructor(private cdr: ChangeDetectorRef) {
     if (this.isSupported) {
       (navigator as NavigatorWithContacts).contacts.getProperties().then((props) => {
         this.availableProps = props;
-        this.cdr.markForCheck();
+        this.cdr.detectChanges();
       });
     }
   }
 
   public async selectContacts(): Promise<void> {
     if (this.isSupported) {
-      await (navigator as NavigatorWithContacts).contacts.select(this.selectedProps, { multiple: this.multiple })
-        .then((contacts) => {
-          this.contacts = contacts;
-          this.cdr.markForCheck();
-        });
+      const contacts = await (navigator as NavigatorWithContacts).contacts.select(
+        this.selectedProps,
+        { multiple: this.multiple },
+      );
+
+      this.contacts = contacts;
+      this.cdr.detectChanges();
     }
   }
 }
