f90a5f318de5869512ed75d5276bfe12bf75accf
Stefan Nedelchev <hardmaster.92@hotmail.com>
2022-05-05 16:25:22 +0300

    use safe url for contact icons

--- src/app/contacts/contact-item/contact-item.component.ts
+++ src/app/contacts/contact-item/contact-item.component.ts
@@ -1,6 +1,7 @@
 import {
   ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnInit,
 } from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { ContactResult } from '../model';
 
 @Component({
@@ -11,13 +12,13 @@ import { ContactResult } from '../model';
 })
 export class ContactItemComponent implements OnInit {
   @Input() contact!: ContactResult;
-  public iconUrl?: string;
+  public iconUrl?: SafeUrl;
 
-  constructor(private cdr: ChangeDetectorRef) { }
+  constructor(private cdr: ChangeDetectorRef, private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
     if (this.contact.icon && this.contact.icon.length > 0) {
-      this.iconUrl = URL.createObjectURL(this.contact.icon[0]);
+      this.iconUrl = this.sanitizer.bypassSecurityTrustUrl(URL.createObjectURL(this.contact.icon[0]));
       this.cdr.markForCheck();
     }
   }
